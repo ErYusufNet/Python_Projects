7902f6ad9fb062d11957ba6627cacbb59f864bb2
YusufEr <yusufer.net@hotmail.com>
2025-05-21 20:47:42 +0300

    Calculator App

--- 1-CalculatorApp.py
+++ 1-CalculatorApp.py
@@ -4,14 +4,16 @@ num1 = float(input("Enter a first number: "))
 num2 = float(input("Enter a second number: "))
 if operator == "+":
     result = num1+ num2
-    print(result)
+    print(round(result))
 elif operator == "-":
     result = num1- num2
-    print(result)
+    print(round(result))
 elif operator == "*":
     result = num1* num2
-    print(result)
+    print(round(result))
 elif operator == "/":
     result = num1/num2
-    print(result)
+    print(round(result))
+else:
+    print(f"{operator} is not valid! ")
 
